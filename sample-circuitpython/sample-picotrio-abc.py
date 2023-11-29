c4cddbca2c56f772fd15ecdc1d4ebb837bfacd1e
Akira Ouchi <akkiesoft@marokun.net>
2023-11-29 11:18:59 +0900

    update sample-picotrio-abc.py
    * replace some arrays with tuple
    * support to sending string
    * change the logic in main loop

--- sample-circuitpython/sample-picotrio-abc.py
+++ sample-circuitpython/sample-picotrio-abc.py
@@ -7,12 +7,13 @@ from time import sleep
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
-led = [
+led = (
     DigitalInOut(board.LED_R),
     DigitalInOut(board.LED_G),
     DigitalInOut(board.LED_B)
-]
+)
 for l in led:
     l.direction = Direction.OUTPUT
     # tiny2040 LED is active low
@@ -22,38 +23,50 @@ keyboard = 0
 while not keyboard:
     try:
         keyboard = Keyboard(usb_hid.devices)
+        layout = KeyboardLayoutUS(keyboard)
     except:
         pass
     sleep(1)
 
-keycodes = [
-    [Keycode.A],
-    [Keycode.B],
-    [Keycode.C]
-]
-button = [
+keycodes = (
+    ("Hello", Keycode.ENTER),
+    (Keycode.A,),
+    (Keycode.B,),
+)
+button = (
     DigitalInOut(board.GP4),
     DigitalInOut(board.GP5),
     DigitalInOut(board.GP6)
-]
+)
 for b in button:
     b.switch_to_input(pull=Pull.UP)
-buttons = len(button)
+sw = [0] * len(button)
 
-released = 1
+def send_key(key):
+    repeat_block = False
+    try:
+        for k in key:
+            if (type(k) is str):
+                repeat_block = True
+                layout.write(k)
+            else:
+                keyboard.press(k)
+            sleep(0.05)
+        if repeat_block:
+            keyboard.release_all()
+    except:
+        pass
+
 while True:
-    no_push = 0
     for i,b in enumerate(button):
-        if b.value:
-            no_push += 1
-            continue
-        led[i].value = 0
-        released = 0
-        for k in keycodes[i]:
-            keyboard.press(k)
-            sleep(0.01)
-    if no_push == buttons and not released:
-        for l in led:
-            l.value = 1
-        keyboard.release_all()
-        released = 1
+        if not b.value:
+            if not sw[i]:
+                send_key(keycodes[i])
+                sw[i] = 1
+                led[i].value = 0
+        else:
+            if sw[i]:
+                keyboard.release_all()
+                sw[i] = 0
+                for l in led:
+                    l.value = 1
